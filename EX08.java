b93d010d81a610982c648dc98160654fbfc3005a
Martinez-LMS <leo.ooosla@gmail.com>
2024-03-15 22:02:54 -0300

    Ex resolvido

---
+++ EX08.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EX08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero >= 50) {
+            System.out.println("O número é maior ou igual a 50.");
+        } else {
+            System.out.println("O número é menor que 50.");
+        }
+
+    }
+}
