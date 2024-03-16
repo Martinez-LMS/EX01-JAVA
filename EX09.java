62c1ccb104e2f9a72c3e758d5819a83235f29d62
Martinez-LMS <leo.ooosla@gmail.com>
2024-03-15 22:04:42 -0300

    Ex resolvido

---
+++ EX09.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EX09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número (A): ");
+        int numeroA = scanner.nextInt();
+        System.out.print("Digite o segundo número (B): ");
+        int numeroB = scanner.nextInt();
+
+        if (numeroA == numeroB) {
+            System.out.println("Os números são iguais.");
+        } else {
+            System.out.println("Os números são diferentes.");
+
+            if (numeroA > numeroB) {
+                System.out.println("O número " + numeroA + " é o maior.");
+            } else {
+                System.out.println("O número " + numeroB + " é o maior.");
+            }
+        }
+
+    }
+}
