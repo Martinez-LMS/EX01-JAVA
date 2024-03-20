bebc91218f764008d43d03300484d1cfbe42c875
Martinez-LMS <leo.ooosla@gmail.com>
2024-03-19 21:13:02 -0300

    Ex resolvido

---
+++ EX14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EX14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor do primeiro termo (a1): ");
+        int a1 = input.nextInt();
+
+        System.out.print("Digite o valor da razão (r): ");
+        int r = input.nextInt();
+
+        System.out.print("Digite a ordem do termo desejado (n): ");
+        int n = input.nextInt();
+
+
+        int an = a1 + (n - 1) * r;
+
+        System.out.println("O " + n + "º termo da Progressão Aritmética é: " + an);
+
+
+    }
+}
