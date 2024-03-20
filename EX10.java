d174ce7e8c0d11f0e83e908cbd566a6972639ba9
Martinez-LMS <leo.ooosla@gmail.com>
2024-03-19 21:11:43 -0300

    Ex resolvido

---
+++ EX10.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EX10 {
+    public static void main(String[] args) {
+        Scanner imprimir = new Scanner(System.in);
+
+        System.out.println("Digite o numero de 1 a 5: ");
+        int numero = imprimir.nextInt();
+
+        switch (numero) {
+            case  1:
+                System.out.println("UM");             
+                break;
+            case 2:
+                System.out.println("DOIS");             
+                break;
+            case 3:
+                System.out.println("TRES");             
+                break;
+            case 4:
+                System.out.println("QUATRO");             
+                break;
+            case 5:
+                System.out.println("CINCO");             
+                break;
+        
+            default:
+                System.out.println("Numero invalido !");
+                break;
+        }
+    }
+}
